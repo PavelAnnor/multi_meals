c0858c588c58387024df71f7c78902f59c830284
PavelAnnor <annorpavel@gmail.com>
2026-07-29 19:31:39 -0400

    additional work on home design + pagnation to divide things into different components

---
+++ src/components/ui/custom/ImageDelimiter.jsx
@@ -0,0 +1,23 @@
+
+export default function ImageDelimiter({num, delimiterImgSrc}){
+
+
+    const images = []
+    for(let i =0; i<num;i++){
+
+        images.push(<img src={delimiterImgSrc} key = {`${i}+${delimiterImgSrc}`}></img>);
+
+    }
+
+    return (
+      <div className="flex border-amber-300 border-4 h-11  ml-auto mr-auto justify-evenly gap-5">
+       {images}
+      </div>
+    );
+
+
+
+
+
+}
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,21 @@
 
-
+import { MainAndSubTextSection } from "../components/ui/custom/MainAndSubTextSection";
+import ImageDelimiter from "../components/ui/custom/ImageDelimiter";
 
 export default function Home({mainText,subText, delimiterImgSrc}){
 
 
     return (
-      <main className="border-red-400 border-4 w-95/100 h-72 m-auto mt-10 rounded-4xl flex flex-col ">
-        <TextSection mainText={mainText} subText={subText}></TextSection>
-        <div className="flex border-amber-300 border-4 h-11 w-2/10 ml-auto mr-auto justify-evenly">
-          <img src={delimiterImgSrc}></img>
-          <img src={delimiterImgSrc}></img>
-          <img src={delimiterImgSrc}></img>
-        </div>
+      <main className="border-red-400 border-4 w-95/100 m-auto mt-10 rounded-4xl flex flex-col ">
+        <MainAndSubTextSection
+          mainText={mainText}
+          subText={subText}
+        ></MainAndSubTextSection>
+        <ImageDelimiter delimiterImgSrc={delimiterImgSrc} num={3}></ImageDelimiter>
+
+    
       </main>
     );
 }
 
 
-function TextSection({mainText,subText}){
-    return (
-      <div className="border-green-300 border-4 text-center flex flex-col p-6 gap-5 figtree-font w-full">
-        <h2 className="text-6xl">{mainText}</h2>
-        <p className="text-2xl">{subText}</p>
-      </div>
-    );
-}
